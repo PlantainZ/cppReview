da9032f24dd8f47ef288bce5a105ad987fbc3762
PlantainZ <593525228@qq.com>
2025-04-01 20:33:27 +0800

    DS_InsertSort completion
    20250401,completion plan action !!!嘻嘻嘻

--- C++_Algorithm/WinterSolstice/01_InsertSort.cpp
+++ C++_Algorithm/WinterSolstice/01_InsertSort.cpp
@@ -1,7 +1,34 @@
-//DS：插入排序
-
 #include<stdio.h>
 #include<iostream>
 #include<iomanip>
 using namespace std;
 
+// 插入排序 【运行正常】
+
+void main() {
+    // 可更改设置 ======================
+    int a[8] = { 5,8,9,3,21,4,7,6 };
+    int tmp;
+    int n = 8;
+    int i, j;
+
+    // 初始数据=========================
+    for (i = 0; i < n; i++)
+        cout << "a_original：" << a[i] << endl;
+
+    // 插入排序==========================
+
+    for (i = 1; i < n; i++) {
+        tmp = a[i - 1];
+        if (a[i] < tmp) {
+            tmp = a[i];
+
+            for (j = i - 1; a[j] > tmp; j--) a[j + 1] = a[j];
+            a[j + 1] = tmp;
+        }
+    }
+
+    // 最终结果===========================
+    for (i = 0; i < n; i++)
+        cout << "a插入排序后的样子：" << a[i] << endl;
+}
---
+++ C++_Algorithm/WinterSolstice/02_BinarySearchSort.cpp
@@ -0,0 +1,11 @@
+//DS£∫≤Â»Î≈≈–Ú
+
+#include<stdio.h>
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+
+}
+
